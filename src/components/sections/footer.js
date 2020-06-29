8fa88bf9df6158d3e08308430a371bffe299c881
newt0 <py_lambda@yahoo.co.jp>
2020-06-29 22:35:39 +0900

    fix: Footer修正

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -36,7 +36,7 @@ const Footer = () => (
       </FooterColumn>
     </FooterColumnContainer>
     <BrandContainer>
-      <Logo>Finance</Logo>
+      <Logo>SaasCall</Logo>
     </BrandContainer>
   </FooterWrapper>
 )
