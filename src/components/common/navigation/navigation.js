dbd1b01992b57935d4649b66170294729ad34b7e
newt0 <py_lambda@yahoo.co.jp>
2020-06-29 22:34:51 +0900

    fix: Nav修正

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -77,7 +77,7 @@ export default class Navigation extends Component {
           <Brand>
             <Scrollspy offset={-64} item={["top"]} currentClassName="active">
               <AnchorLink href="#top" onClick={this.closeMobileMenu}>
-                Finance
+                SaasCall
               </AnchorLink>
             </Scrollspy>
           </Brand>
@@ -96,7 +96,7 @@ export default class Navigation extends Component {
 
           <Mobile hide>{this.getNavList({})}</Mobile>
           <ActionsContainer>
-            <button>Sign up</button>
+            <button>先行登録</button>
           </ActionsContainer>
         </StyledContainer>
         <Mobile>
